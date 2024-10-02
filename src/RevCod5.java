9773563233f71d42f4f29aaa50d4af8fadc9b278
Libertad <lrivascedillo@gmail.com>
2024-10-02 17:06:09 -0600

    Se corrigieron errores del codigo

--- src/RevCod5.java
+++ src/RevCod5.java
@@ -1,28 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * Este codigo determina si un número es afortunado o no.
+ * Un número se considera afortunado si tiene más dígitos de 3, 7, 8 o 9
+ * que otros números
+ */
 
 public class RevCod5 {
-	Scanner s = new Scanner();
-    System.out.print("Introduzca un número: ');
+	
+	public static void main (String[]args) {
+	Scanner s = new Scanner(System.in);
+	
+    System.out.print("Introduzca un número: ");
     String ni = s.nextLine();
-    int c = ni;
+    
+    int c = Integer.parseInt(ni);
     
     int afo = 0;
     int noAfo = 0;
     
-    while (ni > 0) {
-	  int digito = (int)(ni % 10);
+    int numero = c;
+    
+    while (numero > 0) {
+	  int digito = numero % 10;
       if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
 		afo++;
       } else {
 		noAfo++;
-	  
-	  ni /= 10;
+      }//else 
+      numero /= 10;
     }
     if (afo > noAfo) {
-      System.out.prinln("El " + c + " es un número afortunado.");
+      System.out.println("El " + c + " es un número afortunado.");
     } else {
       System.out.println("El " + c + " no es un número afortunado.");
-    }
+    }//else 
     
-  }
-  
+    s.close();
+	}//main
 }//RevCod5
